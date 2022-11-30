5160e38577fcff8ecb8ffe75330dcc91a947294d
himanshuparmar121 <parmarhimanshu360@gmail.com>
2022-11-30 23:34:45 +0530

    Added ProductObject model for product mapping

---
+++ Product/Model/ProductObjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Model
+{
+    public class ProductObjects
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Products { get; set; }
+        [Required]
+        public double Quantity { get; set; }
+    }
+}
