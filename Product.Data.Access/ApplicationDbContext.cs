e1ae8f026c3c1d5624e5a390929962721fab045c
Himanshu Parmar <parmarhimanshu360@gmail.com>
2022-12-03 17:50:52 +0530

    Added database connection and created tables by Code first approach and added API service project

--- Product.Data.Access/ApplicationDbContext.cs
+++ Product.Data.Access/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Product.DbMigrations
 {
-    class ApplicationDbContext 
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        DbSet<Item> Items { get; set; }
+        DbSet<ItemObjects> ItemObjects { get; set; }
     }
 }
